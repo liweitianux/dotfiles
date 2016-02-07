2cbefa4da6f522e273e3df121f272d205e96e976
Aaron LI <aaronly.me@gmail.com>
2016-02-07 23:57:04 +0800

    small updates

--- _config/openbox/autostart.sh
+++ _config/openbox/autostart.sh
@@ -1,16 +1,24 @@
 #!/bin/sh
+#
+# ~/.config/openbox/autostart.sh
+#
+# Aaron LI
+# 2016-01-31
+#
 
-xset -b
+# disable X beep
+xset b off
 
-#feh --bg-fill "$HOME/pictures/wallpapers/.current" &
+# set wallpaper
 nitrogen --restore &
 
-pgrep -x urxvtd || urxvtd -q -f -o &
-pgrep -x fcitx || fcitx &
-pgrep -x tint2 || tint2 &
-pgrep -x xcompmgr || xcompmgr &
+pgrep -x redshift     || redshift &
+pgrep -x urxvtd       || urxvtd -q -f -o &
+pgrep -x fcitx        || fcitx &
+pgrep -x tint2        || tint2 &
+pgrep -x xcompmgr     || xcompmgr &
 pgrep -x xscreensaver || xscreensaver -no-splash &
-pgrep -x mpd || mpd &
+pgrep -x mpd          || mpd &
 
 # pgrep -x parcellite || parcellite &
 #xfce4-power-manager &
