016d716a2d6857b34aafd790076fc6ce6ab9772c
Aaron LI <aaronly.me@gmail.com>
2016-03-07 16:01:53 +0800

    _emacs.d/personal: add 30-modes.el

---
+++ _emacs.d/personal/30-modes.el
@@ -0,0 +1,17 @@
+;;; ~/.emacs.d/personal/10-config.el --- Custom Emacs configurations
+;;
+;; -*- mode: emacs-lisp -*-
+;; vim: set ts=8 sw=2 tw=0 fenc=utf-8 ft=lisp:
+;;
+;; Aaron LI
+;; 2016-02-28
+;;
+
+;;; Commentary:
+;; Extra Emacs modes.
+
+;;; Code:
+
+;; julia
+(prelude-auto-install "\\.jl\\'" julia-mode julia-mode)
+
