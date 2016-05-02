1dc93cc76a88d8f0aba6c625858c49889d838b5c
Aaron LI <aaronly.me@gmail.com>
2016-05-02 15:11:16 +0800

    _zsh: reorder history.zsh; intergrate "percol"

---
+++ _zsh/50-history.zsh
@@ -0,0 +1,30 @@
+#
+# zsh/history.zsh
+#
+
+HISTFILE=~/.zsh_history
+
+HISTSIZE=10000
+SAVEHIST=9000
+# ignore these commands without arguments
+HISTIGNORE="cd:ls:ll"
+
+
+# Combine history search with `percol`
+# Credit: https://github.com/mooz/percol
+if exists percol; then
+    function percol_select_history() {
+        local tac
+        exists gtac && tac="gtac" || {
+                exists tac && tac="tac" || {
+                        tac="tail -r" } }
+        BUFFER=$(fc -l -n 1 | eval ${tac} | percol --query "${LBUFFER}")
+        CURSOR=${#BUFFER}  # move cursor
+        zle -R -c  # refresh
+    }
+
+    zle -N percol_select_history
+    bindkey '^R' percol_select_history
+fi
+
+# vim: set ts=8 sw=4 tw=0 fenc=utf-8 ft=zsh: #
