3d93c068135717c152d917085611fdae9b6a78be
Aaron LI <aaronly.me@gmail.com>
2016-05-15 21:29:51 +0800

    Move _spacemacs to _spacemacs.d/init.el

--- _spacemacs.d/init.el
+++ _spacemacs.d/init.el
@@ -1,26 +1,35 @@
 ;; -*- mode: emacs-lisp -*-
 ;; vim: set ts=8 sw=4 tw=0 fenc=utf-8 ft=lisp:
 ;;
+;; Spacemacs: A community-driven Emacs distribution
+;; The best editor is neither Emacs nor Vim, it's Emacs *and* Vim!
+;; http://spacemacs.org/
+;; https://github.com/syl20bnr/spacemacs
+;;
+;; ~/.spacemacs
+;; ~/.spacemacs.d/init.el
+;;
 ;; Aaron LI
 ;; Created: 2016-04-30
-;; Updated: 2016-05-12
+;; Updated: 2016-05-15
 ;;
 
 
 (defun dotspacemacs/layers ()
   "Configuration Layers declaration.
-You should not put any user code in this function besides modifying the variable
-values."
+  You should not put any user code in this function besides modifying
+  the variable values."
   (setq-default
-   ;; Base distribution to use. This is a layer contained in the directory
-   ;; `+distribution'. For now available distributions are `spacemacs-base'
-   ;; or `spacemacs'. (default 'spacemacs)
+   ;; Base distribution to use.
+   ;; This is a layer contained in the directory `+distribution'.
+   ;; For now available distributions are `spacemacs-base' or `spacemacs'.
    dotspacemacs-distribution 'spacemacs
    ;; List of additional paths where to look for configuration layers.
-   ;; Paths must have a trailing slash (i.e. `~/.mycontribs/')
-   dotspacemacs-configuration-layer-path '()
-   ;; List of configuration layers to load. If it is the symbol `all' instead
-   ;; of a list then all discovered layers will be installed.
+   ;; Paths must have a trailing slash (i.e. `~/.mycontribs/').
+   dotspacemacs-configuration-layer-path '("~/.spacemacs.d/private/")
+   ;; List of configuration layers to load.
+   ;; If it is the symbol `all' instead of a list then all discovered
+   ;; layers will be installed.
    dotspacemacs-configuration-layers
    '(
      ;; ----------------------------------------------------------------
@@ -34,7 +43,7 @@ values."
                       auto-completion-complete-with-key-sequence-delay 0.1
                       auto-completion-enable-snippets-in-popup t
                       auto-completion-enable-help-tooltip t
-                      ;; Provided by `company-statistics' when `company' used
+                      ;; Provided by `company-statistics'
                       auto-completion-enable-sort-by-usage t)
      ;; better-defaults
      c-c++
@@ -61,28 +70,28 @@ values."
      ;; version-control
      )
    ;; List of additional packages that will be installed without being
-   ;; wrapped in a layer. If you need some configuration for these
-   ;; packages, then consider creating a layer. You can also put the
+   ;; wrapped in a layer.  If you need some configuration for these
+   ;; packages, then consider creating a layer.  You can also put the
    ;; configuration in `dotspacemacs/user-config'.
    dotspacemacs-additional-packages '()
-   ;; A list of packages and/or extensions that will not be install and loaded.
+   ;; List of packages/extensions that will not be installed or loaded.
    dotspacemacs-excluded-packages '()
-   ;; If non-nil spacemacs will delete any orphan packages, i.e. packages that
-   ;; are declared in a layer which is not a member of
-   ;; the list `dotspacemacs-configuration-layers'. (default t)
+   ;; If non-nil, spacemacs will delete any orphan packages, i.e. packages
+   ;; that are declared in a layer which is not a member of the list
+   ;; `dotspacemacs-configuration-layers'. (default t)
    dotspacemacs-delete-orphan-packages t))
 
 (defun dotspacemacs/init ()
   "Initialization function.
-This function is called at the very startup of Spacemacs initialization
-before layers configuration.
-You should not put any user code in there besides modifying the variable
-values."
+  This function is called at the very startup of Spacemacs initialization
+  before layers configuration.
+  You should not put any user code in there besides modifying the variable
+  values."
   ;; This setq-default sexp is an exhaustive list of all the supported
   ;; spacemacs settings.
   (setq-default
-   ;; If non nil ELPA repositories are contacted via HTTPS whenever it's
-   ;; possible. Set it to nil if you have no way to use HTTPS in your
+   ;; If non-nil, ELPA repositories are contacted via HTTPS whenever it's
+   ;; possible.  Set it to nil if you have no way to use HTTPS in your
    ;; environment, otherwise it is strongly recommended to let it set to t.
    ;; This variable has no effect if Emacs is launched with the parameter
    ;; `--insecure' which forces the value of this variable to nil.
@@ -93,25 +102,25 @@ values."
    ;; If non nil then spacemacs will check for updates at startup
    ;; when the current branch is not `develop'. (default t)
    dotspacemacs-check-for-update t
-   ;; One of `vim', `emacs' or `hybrid'. Evil is always enabled but if the
-   ;; variable is `emacs' then the `holy-mode' is enabled at startup. `hybrid'
-   ;; uses emacs key bindings for vim's insert mode, but otherwise leaves evil
-   ;; unchanged. (default 'vim)
+   ;; One of `vim', `emacs' or `hybrid'.  Evil is always enabled but if
+   ;; the variable is `emacs' then the `holy-mode' is enabled at startup.
+   ;; `hybrid' uses emacs key bindings for vim's insert mode, but otherwise
+   ;; leaves evil unchanged. (default 'vim)
    dotspacemacs-editing-style 'vim
    ;; If non nil output loading progress in `*Messages*' buffer. (default nil)
    dotspacemacs-verbose-loading nil
-   ;; Specify the startup banner. Default value is `official', it displays
-   ;; the official spacemacs logo. An integer value is the index of text
+   ;; Specify the startup banner.  Default value is `official', it displays
+   ;; the official spacemacs logo.  An integer value is the index of text
    ;; banner, `random' chooses a random text banner in `core/banners'
-   ;; directory. A string value must be a path to an image format supported
+   ;; directory.  A string value must be a path to an image format supported
    ;; by your Emacs build.
    ;; If the value is nil then no banner is displayed. (default 'official)
    dotspacemacs-startup-banner 'random
-   ;; List of items to show in the startup buffer. If nil it is disabled.
+   ;; List of items to show in the startup buffer.  If nil it is disabled.
    ;; Possible values are: `recents' `bookmarks' `projects'.
    ;; (default '(recents projects))
    dotspacemacs-startup-lists '(recents projects)
-   ;; Number of recent files to show in the startup buffer. Ignored if
+   ;; Number of recent files to show in the startup buffer.  Ignored if
    ;; `dotspacemacs-startup-lists' doesn't include `recents'. (default 5)
    dotspacemacs-startup-recent-list-size 5
    ;; Default major mode of the scratch buffer (default `text-mode')
@@ -128,7 +137,7 @@ values."
                          zenburn)
    ;; If non nil the cursor color matches the state color in GUI Emacs.
    dotspacemacs-colorize-cursor-according-to-state t
-   ;; Default font. `powerline-scale' allows to quickly tweak the mode-line
+   ;; Default font.  `powerline-scale' allows to quickly tweak the mode-line
    ;; size to make separators look not too crappy.
    dotspacemacs-default-font '("Source Code Pro"
                                :size 14
@@ -141,7 +150,7 @@ values."
    ;; (default "M-m")
    dotspacemacs-emacs-leader-key "M-m"
    ;; Major mode leader key is a shortcut key which is the equivalent of
-   ;; pressing `<leader> m`. Set it to `nil` to disable it. (default ",")
+   ;; pressing `<leader> m`.  Set it to `nil` to disable it. (default ",")
    dotspacemacs-major-mode-leader-key ","
    ;; Major mode leader key accessible in `emacs state' and `insert state'.
    ;; (default "C-M-m)
@@ -156,8 +165,8 @@ values."
    ;; (Not implemented) dotspacemacs-distinguish-gui-ret nil
    ;; The command key used for Evil commands (ex-commands) and
    ;; Emacs commands (M-x).
-   ;; By default the command key is `:' so ex-commands are executed like in Vim
-   ;; with `:' and Emacs commands are executed with `<leader> :'.
+   ;; By default the command key is `:' so ex-commands are executed like
+   ;; in Vim with `:' and Emacs commands are executed with `<leader> :'.
    dotspacemacs-command-key ":"
    ;; If non nil `Y' is remapped to `y$'. (default t)
    dotspacemacs-remap-Y-to-y$ t
@@ -176,7 +185,7 @@ values."
    dotspacemacs-auto-save-file-location 'cache
    ;; Maximum number of rollback slots to keep in the cache. (default 5)
    dotspacemacs-max-rollback-slots 5
-   ;; If non nil then `ido' replaces `helm' for some commands. For now only
+   ;; If non nil then `ido' replaces `helm' for some commands.  For now only
    ;; `find-files' (SPC f f), `find-spacemacs-file' (SPC f e s), and
    ;; `find-contrib-file' (SPC f e c) are replaced. (default nil)
    dotspacemacs-use-ido nil
@@ -194,14 +203,14 @@ values."
    ;; Which-key delay in seconds. The which-key buffer is the popup listing
    ;; the commands bound to the current keystroke sequence. (default 0.4)
    dotspacemacs-which-key-delay 0.4
-   ;; Which-key frame position. Possible values are `right', `bottom' and
-   ;; `right-then-bottom'. right-then-bottom tries to display the frame to the
-   ;; right; if there is insufficient space it displays it at the bottom.
-   ;; (default 'bottom)
+   ;; Which-key frame position.  Possible values are `right', `bottom' and
+   ;; `right-then-bottom'.  right-then-bottom tries to display the frame to
+   ;; the right; if there is insufficient space it displays it at the
+   ;; bottom. (default 'bottom)
    dotspacemacs-which-key-position 'bottom
-   ;; If non nil a progress bar is displayed when spacemacs is loading. This
-   ;; may increase the boot time on some systems and emacs builds, set it to
-   ;; nil to boost the loading time. (default t)
+   ;; If non nil a progress bar is displayed when spacemacs is loading.
+   ;; This may increase the boot time on some systems and emacs builds,
+   ;; set it to nil to boost the loading time. (default t)
    dotspacemacs-loading-progress-bar t
    ;; If non nil the frame is fullscreen when Emacs starts up. (default nil)
    ;; (Emacs 24.4+ only)
@@ -223,33 +232,33 @@ values."
    dotspacemacs-inactive-transparency 90
    ;; If non nil unicode symbols are displayed in the mode line. (default t)
    dotspacemacs-mode-line-unicode-symbols t
-   ;; If non nil smooth scrolling (native-scrolling) is enabled. Smooth
-   ;; scrolling overrides the default behavior of Emacs which recenters the
+   ;; If non nil smooth scrolling (native-scrolling) is enabled.  Smooth
+   ;; scrolling overrides the default behavior of Emacs which re-centers the
    ;; point when it reaches the top or bottom of the screen. (default t)
    dotspacemacs-smooth-scrolling t
-   ;; If non nil line numbers are turned on in all `prog-mode' and `text-mode'
-   ;; derivatives. If set to `relative', also turns on relative line numbers.
-   ;; (default nil)
+   ;; If non nil line numbers are turned on in all `prog-mode' and
+   ;; `text-mode' derivatives.  If set to `relative', also turns on relative
+   ;; line numbers. (default nil)
    dotspacemacs-line-numbers t
    ;; If non-nil smartparens-strict-mode will be enabled in programming modes.
    ;; (default nil)
    dotspacemacs-smartparens-strict-mode nil
-   ;; Select a scope to highlight delimiters. Possible values are `any',
-   ;; `current', `all' or `nil'. Default is `all' (highlight any scope and
+   ;; Select a scope to highlight delimiters.  Possible values are `any',
+   ;; `current', `all' or `nil'.  Default is `all' (highlight any scope and
    ;; emphasis the current one). (default 'all)
    dotspacemacs-highlight-delimiters 'all
    ;; If non nil advises quit functions to keep server open when quitting.
    ;; (default nil)
    dotspacemacs-persistent-server nil
-   ;; List of search tool executable names. Spacemacs uses the first installed
-   ;; tool of the list. Supported tools are `ag', `pt', `ack' and `grep'.
-   ;; (default '("ag" "pt" "ack" "grep"))
+   ;; List of search tool executable names.  Spacemacs uses the first
+   ;; installed tool of the list.  Supported tools are `ag', `pt', `ack'
+   ;; and `grep'. (default '("ag" "pt" "ack" "grep"))
    dotspacemacs-search-tools '("ag" "pt" "ack" "grep")
    ;; The default package repository used if no explicit repository has been
    ;; specified with an installed package.
    ;; Not used for now. (default nil)
    dotspacemacs-default-package-repository nil
-   ;; Delete whitespace while saving buffer. Possible values are `all'
+   ;; Delete whitespace while saving buffer.  Possible values are `all'
    ;; to aggressively delete empty line and long sequences of whitespace,
    ;; `trailing' to delete only the whitespace at end of lines, `changed' to
    ;; delete only whitespace for changed lines or `nil' to disable cleanup.
@@ -259,15 +268,15 @@ values."
 
 (defun dotspacemacs/user-init ()
   "Initialization function for user code.
-It is called immediately after `dotspacemacs/init'.  You are free to put almost
-any user code here.  The exception is org related code, which should be placed
-in `dotspacemacs/user-config'."
+  It is called immediately after `dotspacemacs/init'.
+  You are free to put almost any user code here.  The exception is
+  org related code, which should be placed in `dotspacemacs/user-config'."
   )
 
 (defun dotspacemacs/user-config ()
   "Configuration function for user code.
-This function is called at the very end of Spacemacs initialization after
-layers configuration. You are free to put any user code."
+  This function is called at the very end of Spacemacs initialization after
+  layers configuration. You are free to put any user code."
   ;; Enable `company' globally
   (global-company-mode)
   (setq-default tab-width 4)
