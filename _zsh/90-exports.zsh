71fcdcb32cb4b0740c4c12b706251799b54cfd5c
Aaron LI <aaronly.me@gmail.com>
2016-05-15 15:21:14 +0800

    Move GPG_TTY to shell configuration

--- _zsh/90-exports.zsh
+++ _zsh/90-exports.zsh
@@ -9,6 +9,9 @@ export PAGER='less'
 export LESS='--ignore-case --raw-control-chars'
 
 # Setup terminal, and turn on colors
-export TERM='xterm-256color'
+#export TERM='xterm-256color'
+
+# GPG: see `gpg-agent(1)'
+export GPG_TTY=$(tty)
 
 # vim: set ts=8 sw=4 tw=0 fenc=utf-8 ft=zsh: #
