62cbf8fd34ec955812c1991eaa9aac8e181f0eed
Aaron LI <aaronly.me@gmail.com>
2016-05-23 20:02:31 +0800

    mu4e: update to version git20160523

--- _spacemacs.d/local/mu4e/mu4e-meta.el
+++ _spacemacs.d/local/mu4e/mu4e-meta.el
@@ -2,7 +2,7 @@
 (defconst mu4e-mu-version "0.9.17"
   "Required mu binary version; mu4e's version must agree with this.")
 
-(defconst mu4e-builddir "/home/aly/build/mu"
+(defconst mu4e-builddir "/home/aly/devel/mu"
   "Top-level build directory.")
 
 (defconst mu4e-doc-dir "/home/aly/local/mu/share/doc/mu"
