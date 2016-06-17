3ee86078aa81942decf2e6063f8cc393f39dae55
Aaron LI <aaronly.me@outlook.com>
2016-06-17 10:32:40 +0800

    Update gpg-agent settings

--- _zsh/90-exports.zsh
+++ _zsh/90-exports.zsh
@@ -11,7 +11,9 @@ export LESS='--ignore-case --raw-control-chars'
 # Setup terminal, and turn on colors
 #export TERM='xterm-256color'
 
-# GPG: see `gpg-agent(1)'
+# GnuPG: see 'gpg-agent(1)'
 export GPG_TTY=$(tty)
+# Refresh 'gpg-agent' tty in case user switches into an X session
+gpg-connect-agent updatestartuptty /bye >/dev/null
 
 # vim: set ts=8 sw=4 tw=0 fenc=utf-8 ft=zsh: #
