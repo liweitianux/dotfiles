3d12a50877a09c9c58e4963c46675ca21bf575b6
Aaron LI <aaronly.me@outlook.com>
2016-12-06 01:52:41 +0800

    _zsh: Remove "gpg-connect-agent"

--- _zsh/90-exports.zsh
+++ _zsh/90-exports.zsh
@@ -6,9 +6,8 @@
 # Generic environment variables and those needing been set only once
 # should go to `~/.profile'.
 
-# GnuPG: see 'gpg-agent(1)'
+# This `GPG_TTY' variable should be set to the correct TTY where the shell
+# is running.  See also `gpg-agent(1)'
 export GPG_TTY=$(tty)
-# Refresh 'gpg-agent' tty in case user switches into an X session
-gpg-connect-agent updatestartuptty /bye >/dev/null
 
 # vim: set ts=8 sw=4 tw=0 fenc=utf-8 ft=zsh: #
