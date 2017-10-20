dfce3c52ba94ef755a19e08982d1d5af54ddf37d
Aaron LI <aly@aaronly.me>
2017-10-20 08:26:21 +0800

    zsh: Move LS_COLORS set from profile to zsh/90-exports.zsh

--- _zsh/20-colors.zsh
+++ _zsh/20-colors.zsh
@@ -16,12 +16,5 @@ export PR_RED PR_GREEN PR_YELLOW PR_BLUE PR_WHITE PR_BLACK
 export PR_BOLD_RED PR_BOLD_GREEN PR_BOLD_YELLOW PR_BOLD_BLUE
 export PR_BOLD_WHITE PR_BOLD_BLACK
 
-# colors for BSD ls
-if [[ -n "${IS_BSD}" ]]; then
-    export CLICOLOR=1
-    #export LSCOLORS=Gxfxcxdxbxegedabagacad
-    export LSCOLORS=exfxcxdxbxegedabagacad
-fi
-
 
 # vim: set ts=8 sw=4 tw=0 fenc=utf-8 ft=zsh: #
--- _zsh/90-exports.zsh
+++ _zsh/90-exports.zsh
@@ -9,5 +9,18 @@
 # This `GPG_TTY' variable should be set to the correct TTY where the shell
 # is running.  See also `gpg-agent(1)'
 export GPG_TTY=$(tty)
+# colors for BSD ls
+if [[ -n "${IS_BSD}" ]]; then
+    export CLICOLOR=1
+    export LSCOLORS=exfxcxdxbxegedabagacad
+fi
+
+# Color setup for `ls': `LS_COLORS'
+# NOTE: For unknown reason, the `LS_COLORS' variable get overridden when
+#       it is set in `~/.profile'.
+if exists dircolors; then
+    eval $(dircolors -b)
+fi
+
 
 # vim: set ts=8 sw=4 tw=0 fenc=utf-8 ft=zsh: #
