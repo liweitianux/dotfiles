1c2c86aabdb954068bf3be7c0ebf2fe0472223d3
Aaron LI <aly@aaronly.me>
2018-02-03 16:30:34 +0800

    zsh: add alias rm=safe-rm and two more git-related aliases

--- _zsh/90-aliases.zsh
+++ _zsh/90-aliases.zsh
@@ -22,12 +22,17 @@ alias la='ls -lAh'
 alias ll='ls -lh'
 alias lsa='ls -lah'
 
+# Do not use `GREP_OPTIONS`
 alias grep='grep --color=auto'
 
 # Git
 alias gita='git add'
 alias gitc='git commit'
 alias gitd='git diff'
+alias gitl='git log'
 alias gits='git status'
+alias gitsl='git shortlog'
+
+exists "safe-rm" && alias rm=safe-rm
 
 # vim: set ts=8 sw=4 tw=0 fenc=utf-8 ft=zsh: #
