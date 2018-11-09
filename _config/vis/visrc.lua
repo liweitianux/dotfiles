17b256c4c04931a1c8f7c85a91b3d30e575da77f
Aaron LI <aly@aaronly.me>
2018-11-10 00:06:14 +0800

    Add basic config for vis (https://github.com/martanne/vis)

---
+++ _config/vis/visrc.lua
@@ -0,0 +1,17 @@
+--[[
+~/.config/vis/visrc.lua
+2018-09-05
+--]]
+
+-- load standard vis module, providing parts of the Lua API
+require('vis')
+
+vis.events.subscribe(vis.events.INIT, function()
+	-- Your global configuration options
+	vis:command('set theme base16-solarized-light')
+end)
+
+vis.events.subscribe(vis.events.WIN_OPEN, function(win)
+	-- Your per window configuration options e.g.
+	-- vis:command('set number')
+end)
